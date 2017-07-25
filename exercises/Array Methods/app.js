c2d3d4282ea20a37fc7ab0a2de26c2c09770b1f5
rmhowell <rachel.mhowell96@gmail.com>
2017-07-25 16:14:26 -0600

    update array methods and add design a blog

--- exercises/Array Methods/app.js
+++ exercises/Array Methods/app.js
@@ -1,2 +1,42 @@
 var fruit = ["banana", "apple", "orange", "watermelon"];  
-var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];  
+
+vegetables.pop();
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+fruit.shift();
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+var index = fruit.indexOf("orange");
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+fruit.push(index);
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+console.log(vegetables.length);
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+vegetables.push(vegetables.length);
+console.log("fruit: ", fruit);  
+console.log("vegetables: ", vegetables);  
+
+var food = fruit.concat(vegetables);
+console.log("food: ", food);
+
+
+food.splice(4, 2);
+console.log("food: ", food);
+  
+
+food.reverse();
+console.log("food: ", food);
+
+
+food.toString();
+console.log("food: ", food);
+
