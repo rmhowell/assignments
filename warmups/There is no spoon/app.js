a9a8576a627106e5a24bee8b75aa48b80c111fb5
rmhowell <rachel.mhowell96@gmail.com>
2017-07-26 15:43:43 -0600

    There is no spoon 1

--- warmups/There is no spoon/app.js
+++ warmups/There is no spoon/app.js
@@ -1,34 +1,85 @@
-/*book
-spoon
-keys
-food
-laptop
-backpack
-person
-*/
+var room = {
+    lightsOn: false,
+    walls: "white",
+    furniture: ["bed", "desk", "dresser"],
+    clean: true,
+    timeToClean: function() {
+        if(clean === false) {
+            console.log("Time to clean")
+        }
+    }
+};
+
+var windows = {
+    open: false,
+    numWindows: 2,
+};
+
+var closet = {
+    stuff: ["clothes", "snowboard", "shoes", "guitar"]
+};
+
+var bookshelf = {
+    books: [
+    {
+        title: "Airframe",
+        author: "Michael Crichton",
+        numPages: 473,
+        read: true,
+    },
+    {
+        title: "And the Mountain Echoes",
+        author: "Khaled Hosseini",
+        numPages: 402,
+        read: true,
+        },
+    {
+        title: "Catch 22",
+        author: "Joseph Heller",
+        numPages: 567,
+        read: false,
+    }
+  ],
+    numShelves: 4,
+};
 
 var person = {
-  name: ['Rachel', 'Smith'],
+  name: ['Rachel', 'Howell'],
   age: 21,
   gender: 'female',
-  interests: ['music', 'skiing'],
+  interests: ['music', 'skiing', "reading"],
   bio: function() {
-    alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+    console.log(this.name[0] + " " + this.name[1] + " is " + this.age + " years old. She likes " + this.interests[0] + " and " + this.interests[1] + "." + "She also likes " + this.interests[2] + ".");
   },
   greeting: function() {
-    alert('Hi! I\'m ' + this.name[0] + '.');
+    console.log("Hi, I'm " + this.name[0] + ".");
   }
 };
 
-
 var backpack = {
-    contents: ["pen", "laptop", "notebook"],
-    color: "blue",
-}
+    contents: ["laptop", "pens", "notebook", "food", "charger"]
+};
 
+var keys = {
+    type ["house", "car"]
+};
 
-var book = {
-    titles: [],
-    author: ,
-    numBooks: 
+var food = {
+    yummy: true,
+    enough: false, 
+};
+
+var laptop = {
+    type: "MacBook Pro",
+    color: "Space Grey",
+};
+
+var netflix = {
+    shows: [],
+    stillWatching: = true,
+    autoPlay: function() {
+        if(stillWatching === true) {
+            console.log("Resume Play")
+        }
+    }
 }
