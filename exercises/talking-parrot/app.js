e1b482ed39d867d29cd0366d597b18dbf2bc2053
rmhowell <rachel.mhowell96@gmail.com>
2017-08-03 09:10:54 -0600

    talking parrot warmup first commit

---
+++ exercises/talking-parrot/app.js
@@ -0,0 +1,11 @@
+// We have a loud talking parrot. Write a function where the 'hour' parameter is the current hour in the range 0..23, and the 'talking' parameter is a boolean indicating if the parrot is currently talking. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+function trouble(hour, talking) {
+    if (hour < 7 || hour > 20 && talking === true) {
+        console.log("YOU ARE IN TROUBLE!!!!");
+    } else {
+        console.log("You are not in trouble");
+    }
+}
+
+trouble(6, true)
