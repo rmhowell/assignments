cc3d58d614b40ffba81c2240531f527f0886cd56
rmhowell <rachel.mhowell96@gmail.com>
2017-08-07 11:23:12 -0600

    double sum function first commit

---
+++ exercises/sumdouble/app.js
@@ -0,0 +1,10 @@
+function sumDouble(int1, int2) {
+    if(int1 === int2){
+        return (int1 + int2)*2;
+    } else {
+        return int1 + int2;
+    }
+}
+
+console.log(sumDouble(1, 2));
+console.log(sumDouble(5, 5));
