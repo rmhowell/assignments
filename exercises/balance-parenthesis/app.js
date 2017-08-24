d634407db2bdf8ad4f415f5895dc38e1997c93f8
V School Student <student@Vs-Mac-Pro-7.local>
2017-08-24 10:15:44 -0600

    parenthesis balance function with testing

---
+++ exercises/balance-parenthesis/app.js
@@ -0,0 +1,13 @@
+let parenBal = function(str) {
+    let count = 0
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === "(") count++;
+        if(str[i] === ")") count--;
+        if(count < 0) return false;
+    }
+    return count === 0;
+}
+
+console.log(parenBal("()()()()"))
+
+module.exports = parenBal;
